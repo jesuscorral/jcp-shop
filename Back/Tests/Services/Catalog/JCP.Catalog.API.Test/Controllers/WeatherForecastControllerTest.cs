c87a91ead626e9de00086293d7e68296da770437
corralperez <corralperez@gmail.com>
2021-02-27 20:46:26 +0100

    Add catalog project and test project

---
+++ Back/Tests/Services/Catalog/JCP.Catalog.API.Test/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using JCP.Catalog.API.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace JCP.Catalog.API.Test.Controllers
+{
+    public class WeatherForecastControllerTest
+        {
+            private readonly Mock<ILogger<WeatherForecastController>> _mockLogger;
+
+            public WeatherForecastControllerTest()
+            {
+                _mockLogger = new Mock<ILogger<WeatherForecastController>>();
+            }
+
+            [Fact]
+            public void WeatherForecastController_Get_Ok()
+            {
+                // Arrange 
+                var wController = CreateSut();
+
+                // Act
+                var ret = wController.Get();
+
+                // Assert
+                Assert.NotNull(ret);
+            }
+
+            private WeatherForecastController CreateSut()
+            {
+                return new WeatherForecastController(_mockLogger.Object);
+            }
+        }
+}
