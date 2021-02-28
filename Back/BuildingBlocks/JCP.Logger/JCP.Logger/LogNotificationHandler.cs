eec985616e511ee06e1b674fc7a02736d23163cd
corralperez <corralperez@gmail.com>
2021-03-01 00:33:53 +0100

    Improve logger

--- Back/BuildingBlocks/JCP.Logger/JCP.Logger/LogNotificationHandler.cs
+++ Back/BuildingBlocks/JCP.Logger/JCP.Logger/LogNotificationHandler.cs
@@ -7,6 +7,13 @@ namespace JCP.Logger
 {
     public class LogNotificationHandler : INotificationHandler<LogNotification>
     {
+        private static Logger _log;
+
+        public LogNotificationHandler()
+        {
+            _log = Logger.GetInstance();
+        }
+
         public async Task Handle(LogNotification notification, CancellationToken cancellationToken)
         {
             await Task.Run(() => Log(notification), cancellationToken);
@@ -14,9 +21,7 @@ namespace JCP.Logger
 
         private static void Log(LogNotification notification)
         {
-            // TODO - Crear una clase Logger para sobreescribir a Serilog, esa clase será tambien la encargada de escribir en base de datos el log
-            Console.Write("Logeado");
-            //Logger.Log(notification.Message, LogEvents.GeneralMessage, notification.TraceEventType, null, LogCategories.Process);
+            _log.LogTrace(notification);
         }
     }
 }
---
+++ Back/BuildingBlocks/JCP.Logger/JCP.Logger/Logger.cs
@@ -0,0 +1,79 @@
+﻿using Serilog;
+using Serilog.Events;
+using System.Diagnostics;
+
+namespace JCP.Logger
+{
+    public class Logger
+    {
+        private Logger() { }
+
+        private static Logger _instance;
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+            {
+
+            Log.Logger = new LoggerConfiguration()
+                                .MinimumLevel.Debug()
+                                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                                .Enrich.FromLogContext()
+                                .WriteTo.Console()
+                                //.WriteTo.Seq("http://localhost:5341")
+                                .CreateLogger();
+
+                _instance = new Logger();
+            }
+
+            return _instance;
+        }
+
+
+        public void LogTrace(LogNotification notification)
+        {
+            switch (notification.TraceEventType)
+            {
+                case TraceEventType.Information:
+                    Info(notification);
+                    break;
+
+                case TraceEventType.Error:
+                    Error(notification);
+                    break;
+
+                case TraceEventType.Warning:
+                    Warning(notification);
+                    break;
+
+                case TraceEventType.Critical:
+                    Critical(notification);
+                    break;
+
+                default:
+                    Info(notification);
+                    break;
+            }
+        }
+
+        private void Info(LogNotification notification)
+        {
+            Log.Information(notification.Message);
+        }
+
+        private void Error(LogNotification notification)
+        {
+            Log.Error(notification.Message);
+        }
+
+        private void Warning(LogNotification notification)
+        {
+            Log.Warning(notification.Message);
+        }
+
+        private void Critical(LogNotification notification)
+        {
+            Log.Fatal(notification.Message);
+        }
+    }
+}
