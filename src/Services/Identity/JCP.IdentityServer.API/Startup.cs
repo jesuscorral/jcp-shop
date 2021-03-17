5c75889ee21908c4d04b7a1996bdb8f991f4a842
corralperez <corralperez@gmail.com>
2021-03-17 20:27:19 +0100

    Add Identityserver UI

--- src/Services/Identity/JCP.IdentityServer.API/Startup.cs
+++ src/Services/Identity/JCP.IdentityServer.API/Startup.cs
@@ -26,18 +26,22 @@ namespace JCP.IdentityServer.API
                     .AddInMemoryApiScopes(Config.ApiScopes)
                     .AddTestUsers(Config.TestUsers)
                     .AddDeveloperSigningCredential();
+
+            // Add identity server UI
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseStaticFiles();
             app.UseRouting();
+
             app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
         }
     }
 }
