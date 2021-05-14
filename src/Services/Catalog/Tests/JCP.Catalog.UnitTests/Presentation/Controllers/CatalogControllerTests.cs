5c9c177c6be6e83ddd16f634fee6f16f52555467
Jesús <corralperez@gmail.com>
2021-05-14 18:54:03 +0200

    Add unitTests project

---
+++ src/Services/Catalog/Tests/JCP.Catalog.UnitTests/Presentation/Controllers/CatalogControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace JCP.Catalog.UnitTests.Presentation.Controllers
+{
+    public class CatalogControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
