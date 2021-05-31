4fec200a9b19632a4d190a54c94f1a2efdb0b9bb
Jesús <corralperez@gmail.com>
2021-05-31 17:11:34 +0200

    Create ordering BFF

---
+++ src/ApiGateways/JCP.Ordering.BFF/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JCP.Ordering.BFF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
